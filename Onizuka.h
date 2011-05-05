a850f2b6bfd90ea8a071e4d4b75da13609d157fb
K8TIY <moses@blugs.com>
2011-05-05 12:56:47 -0400

    Bump copyright end year.

--- Onizuka.h
+++ Onizuka.h
@@ -1,5 +1,5 @@
 /*
-Copyright © 2005-2010 Brian S. Hall
+Copyright © 2005-2011 Brian S. Hall
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License version 2 as
