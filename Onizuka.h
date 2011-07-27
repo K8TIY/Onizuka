46cc5a05115cadb92009da9d868ebebabad635a2
K8TIY <moses@blugs.com>
2011-07-27 15:51:50 -0400

    License under GPL 2 or later instead of just 2.

--- Onizuka.h
+++ Onizuka.h
@@ -2,7 +2,7 @@
 Copyright © 2005-2011 Brian S. Hall
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 as
+it under the terms of the GNU General Public License version 2 or later as
 published by the Free Software Foundation.
 
 This program is distributed in the hope that it will be useful,
