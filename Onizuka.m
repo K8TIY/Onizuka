700b501392a28aba435642cc764643c847b0cbfc
K8TIY <moses@blugs.com>
2012-02-21 14:49:10 -0500

    Add objc/objc-class.h include to silence LLVM warnings.

--- Onizuka.m
+++ Onizuka.m
@@ -14,6 +14,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 #import "Onizuka.h"
+#import <objc/objc-class.h>
 
 @interface Onizuka (Private)
 -(void)localizeTextView:(NSTextView*)tv;
